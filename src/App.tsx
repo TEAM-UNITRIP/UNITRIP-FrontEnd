7a94f559528632f08eb43c5c9e04d7a58c5d787f
seobbang <seobbangway@naver.com>
2024-07-06 01:48:54 +0900

    init: set Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from "@emotion/react";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import theme from "./styles/theme";
+import Router from "./Router";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <p css={(theme) => ({ color: theme.colors.purple, fontSize: "10rem" })}>
-          App.tsx
-        </p>
+        <Router />
       </ThemeProvider>
     </>
   );
---
+++ src/Main/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <div css={(theme) => ({ color: theme.colors.purple, fontSize: "10rem" })}>
+      main
+    </div>
+  );
+};
+
+export default MainPage;
