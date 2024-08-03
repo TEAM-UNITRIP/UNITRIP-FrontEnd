ade086590f8cb18533207f48ae6cbe45eca06799
Doyun Lee <doyn511@ajou.ac.kr>
2024-08-04 00:41:48 +0900

    feat: asset, image 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,9 @@
-// export { default as LogoIcon } from "./btn_logo.svg";
-// export { default as SearchIcon } from "./ic_search.svg";
+export { default as ArrowDownIcon } from './icon_arrow_down.svg';
+export { default as ArrowLeftIcon } from './icon_arrow_left.svg';
+export { default as CallIcon } from './icon_call_mono.svg';
+export { default as ClockIcon } from './icon_clock_mono.svg';
+export { default as HeartFilledIcon } from './icon_heart_filled.svg';
+export { default as HeartGrayIcon } from './icon_heart_gray.svg';
+export { default as LocationPinIcon } from './icon_location_pin.svg';
+export { default as MapPinIcon } from './icon_map_pin.svg';
+export { default as StarIcon } from './icon_star_filled.svg';
--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,2 +1,1 @@
-// export { default as BrandNameListImg } from './btn_brand_name_list.png';
-// export { default as BuyGiftImg } from './btn_buy/gift.png';
+export { default as RequestImage } from './img_request_outline.png';
