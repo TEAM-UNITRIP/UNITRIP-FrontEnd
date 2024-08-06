593a25870815872d63396c535eaea2dfbc4f23e7
Doyun Lee <doyn511@ajou.ac.kr>
2024-08-06 23:43:56 +0900

    feat: Router 설정 및 asset 적용

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,11 +1,10 @@
 /// <reference types="vite-plugin-svgr/client" />
-// export { default as LogoIcon } from "./btn_logo.svg?react";
-export { default as ArrowDownIcon } from './icon_arrow_down.svg';
-export { default as ArrowLeftIcon } from './icon_arrow_left.svg';
-export { default as CallIcon } from './icon_call_mono.svg';
-export { default as ClockIcon } from './icon_clock_mono.svg';
-export { default as HeartFilledIcon } from './icon_heart_filled.svg';
-export { default as HeartGrayIcon } from './icon_heart_gray.svg';
-export { default as LocationPinIcon } from './icon_location_pin.svg';
-export { default as MapPinIcon } from './icon_map_pin.svg';
-export { default as StarIcon } from './icon_star_filled.svg';
+export { default as ArrowDownIcon } from './icon_arrow_down.svg?react';
+export { default as ArrowLeftIcon } from './icon_arrow_left.svg?react';
+export { default as CallIcon } from './icon_call_mono.svg?react';
+export { default as ClockIcon } from './icon_clock_mono.svg?react';
+export { default as HeartFilledIcon } from './icon_heart_filled.svg?react';
+export { default as HeartGrayIcon } from './icon_heart_gray.svg?react';
+export { default as LocationPinIcon } from './icon_location_pin.svg?react';
+export { default as MapPinIcon } from './icon_map_pin.svg?react';
+export { default as StarIcon } from './icon_star_filled.svg?react';
