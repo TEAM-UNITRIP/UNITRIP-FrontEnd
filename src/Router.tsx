6c3577a93c82ffd094a40c78e6c4196669a663be
Doyun Lee <doyn511@ajou.ac.kr>
2024-09-04 17:16:55 +0900

    feat: 오류 타입 선택 퍼블리싱

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import DetailPage from './views/Detail/pages/DetailPage';
+import ErrorReportPage from './views/ErrorReport/pages/ErrorReportPage';
 import MainPage from './views/Main/pages/MainPage';
 
 const router = createBrowserRouter([
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     element: <MainPage />,
   },
   { path: '/detail', element: <DetailPage /> },
+  { path: '/error-report', element: <ErrorReportPage /> },
   // {
   //   path: "*",
   //   element: <ErrorPage />,
