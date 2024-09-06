4d0af646ffcc3ab5f39b83eefe06ae7c0d2ac725
aazkgh <gg25hp17@naver.com>
2024-09-07 01:26:10 +0900

    feat: Header 공통 컴포넌트 생성

--- src/views/Login/pages/SignUpPage.tsx
+++ src/views/Login/pages/SignUpPage.tsx
@@ -1,12 +1,16 @@
 import { css } from '@emotion/react';
 import { useState } from 'react';
 
+import { HeaderBackIcon } from '@/assets/icon';
 import BottomButton from '@/components/BottomButton';
+import Header from '@/components/Header';
 import SelectRegion from '@/components/SelectRegion';
 import SelectTravelerType from '@/components/SelectTravelerType';
 
 const SignUpPage = () => {
   const [step, setStep] = useState('지역 설정');
+
+  const moveBack = () => {};
 
   const renderItem = () => {
     if (step === '지역 설정') {
@@ -19,6 +23,7 @@ const SignUpPage = () => {
 
   return (
     <>
+      <Header leftIcon={HeaderBackIcon} leftFn={moveBack} />
       <main css={SignUpPageLayout}>{renderItem()}</main>
       <BottomButton text="테스트" clickedFn={setStep} />
     </>
