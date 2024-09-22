8adf39388ecd519cb452ee53eade56d155a00b68
seobbang <seobbangway@naver.com>
2024-09-22 14:54:50 +0900

    feat: 리뷰 필터링 구현

--- src/views/Detail/components/Review.tsx
+++ src/views/Detail/components/Review.tsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 
 import { ReviewResponse } from '@/types/api/review';
 import { isGuideShown } from '@/utils/storageHideGuide';
-import { createInitialFilterState } from '@/views/Search/constants/category';
+import {
+  createInitialFilterState,
+  getFilterList,
+} from '@/views/Search/constants/category';
 import { category } from '@/views/Search/types/category';
 
 import { STORAGE_KEY } from '../constants/localStorageKey';
@@ -16,30 +19,30 @@ import TotalReview from './review/TotalReview';
 import TotalScore from './review/TotalScore';
 
 const REVIEW_DATA: ReviewResponse[] = [
-  // {
-  //   writer: '왕이샹',
-  //   rate: 5,
-  //   description:
-  //     '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
-  //   convenience: ['주차장', '경사로'],
-  //   imgUrl: ['~~~', '~~~~'],
-  // },
-  // {
-  //   writer: '왕이샹',
-  //   rate: 3,
-  //   description:
-  //     '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
-  //   convenience: ['주차장', '경사로'],
-  //   imgUrl: ['~~~', '~~~~'],
-  // },
-  // {
-  //   writer: '왕이샹',
-  //   rate: 2,
-  //   description:
-  //     '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
-  //   convenience: ['주차장', '경사로'],
-  //   imgUrl: ['~~~', '~~~~'],
-  // },
+  {
+    writer: '왕이샹',
+    rate: 5,
+    description:
+      '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
+    convenience: ['주차장', '경사로'],
+    imgUrl: ['~~~', '~~~~'],
+  },
+  {
+    writer: '왕이샹',
+    rate: 3,
+    description:
+      '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
+    convenience: ['주차장', '경사로'],
+    imgUrl: ['~~~', '~~~~'],
+  },
+  {
+    writer: '왕이샹',
+    rate: 2,
+    description:
+      '앱에서 보았던 것과 같이 작품마다 점자표지판으로 설명이 있어 시각장애인도 불편하지 않게 관람이 가능했어요. 오디오 가이드 대여 서비스도 제공하니 필요하신 분들은 꼭 대여해서 쓰세요!!',
+    convenience: ['주차장', '경사로'],
+    imgUrl: ['~~~', '~~~~'],
+  },
 ];
 
 const Review = () => {
@@ -75,6 +78,8 @@ const Review = () => {
     }));
   };
 
+  const selectedFilterList = getFilterList(filterState);
+
   if (REVIEW_DATA.length === 0) return <NoReview />;
 
   return (
@@ -87,7 +92,9 @@ const Review = () => {
         openBottomSheet={openBottomSheet}
       />
       <ul css={reviewCardContainerCss}>
-        {REVIEW_DATA.map((item, idx) => {
+        {REVIEW_DATA.filter(({ convenience }) =>
+          convenience.some((c) => selectedFilterList.includes(c)),
+        ).map((item, idx) => {
           return <ReviewCard key={idx + item.writer + item.rate} {...item} />;
         })}
       </ul>
