bab619952809b423b61a99665f2a7245f3a00544
aazkgh <gg25hp17@naver.com>
2024-09-24 03:58:39 +0900

    fix: DB 선택문 변경

--- src/apis/supabase/postAddFavorite.ts
+++ src/apis/supabase/postAddFavorite.ts
@@ -11,14 +11,14 @@ const toggleFavorite = async () => {
 
   const { data, error: fetchError } = await unitripSupabase
     .from('USER')
-    .select('favorite')
+    .select('favorite_list')
     .eq('kakao_id', kakaoId);
 
   if (fetchError) {
     throw new Error('사용자 정보를 가져오는 데 문제가 발생했습니다');
   }
 
-  const currentFavorites = data[0].favorite || [];
+  const currentFavorites = data[0].favorite_list || [];
   //기존 배열에 해당 장소가 존재하면 제거, 존재하지 않으면 추가
   const updatedFavorites = currentFavorites.includes(place)
     ? currentFavorites.filter((favorite: number) => favorite !== Number(place))
@@ -27,7 +27,7 @@ const toggleFavorite = async () => {
   const { error } = await unitripSupabase
     .from('USER')
     .update({
-      favorite: updatedFavorites,
+      favorite_list: updatedFavorites,
     })
     .eq('kakao_id', kakaoId);
 
