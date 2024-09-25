4c9f2696a8295f2964ad8fc1289a443e00ded0f0
Doyun Lee <doyn511@ajou.ac.kr>
2024-09-25 22:28:39 +0900

    feat: 이미지조회 api 연결

---
+++ src/apis/public/detailImage1.ts
@@ -0,0 +1,36 @@
+/** 이미지정보 조회 API */
+import { detailImage1Res } from '@/types/detailImage1';
+import { Response } from '@/types/public';
+
+import { publicDataClient } from '..';
+
+interface detailImage1Params {
+  numOfRows: number;
+  pageNo: number;
+  MobileOS: 'IOS' | 'AND' | 'WIN' | 'ETC';
+  contentId: number;
+  imageYN: 'Y' | 'N';
+  subImageYN: 'Y' | 'N';
+}
+
+export const getDetailImage1 = async (paramsInfo: detailImage1Params) => {
+  let params = `MobileApp=UNITRIP&_type=json&serviceKey=${import.meta.env.VITE_PUBLIC_DATA_SERVICE_KEY}`;
+
+  for (const [key, value] of Object.entries(paramsInfo)) {
+    params += `&${key}=${value}`;
+  }
+
+  const {
+    data: {
+      response: {
+        body: {
+          items: { item },
+        },
+      },
+    },
+  } = await publicDataClient.get<Response<detailImage1Res>>(
+    `/detailImage1?${params}`,
+  );
+
+  return item;
+};
---
+++ src/types/detailImage1.ts
@@ -0,0 +1,23 @@
+export interface detailImage1ResItem {
+  serialnum: 'string';
+  smallimageurl: 'string';
+  cpyrhtDivCd: 'string';
+  contentid: 'string';
+  imgname: 'string';
+  originimgurl: 'string';
+}
+
+export interface detailImage1Res {
+  header: {
+    resultCode: 'string';
+    resultMsg: 'string';
+  };
+  body: {
+    numOfRows: number;
+    pageNo: number;
+    totalCount: number;
+    items: {
+      item: detailImage1ResItem[];
+    };
+  };
+}
