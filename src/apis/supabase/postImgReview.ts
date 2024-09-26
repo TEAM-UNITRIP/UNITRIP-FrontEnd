1736d074775e525b16c0d6c25b9c528d7660dbe2
aazkgh <gg25hp17@naver.com>
2024-09-27 02:24:17 +0900

    fix: 파일명 필터링 기능 추가

--- src/apis/supabase/postImgReview.ts
+++ src/apis/supabase/postImgReview.ts
@@ -5,16 +5,21 @@ const postImgReview = async (imgs: File[]) => {
 
   // 각 파일을 Supabase Storage에 업로드
   for (const img of imgs) {
+    const cleanedFileName = decodeURIComponent(img.name).replace(
+      /[<>:"/\\|?*]|[\u0400-\u04FF\uAC00-\uD7AF]+/g,
+      '',
+    );
+    console.log(cleanedFileName);
     const { error: uploadError } = await unitripSupabase.storage
       .from('REVIEW_IMAGES')
-      .upload(`${img.name}`, img);
+      .upload(`${cleanedFileName}`, img);
 
     if (uploadError) {
       throw new Error('이미지 업로드 과정에서 에러가 발생했습니다');
     }
 
     // 업로드한 파일의 URL 생성
-    const publicUrl = `${import.meta.env.VITE_STORAGE_URL}/${img.name}`;
+    const publicUrl = `${import.meta.env.VITE_STORAGE_URL}/${cleanedFileName}`;
     imgUrls.push(publicUrl);
   }
 
