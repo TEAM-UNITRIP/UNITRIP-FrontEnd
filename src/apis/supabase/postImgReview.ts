c1aa96e057e1f610c3824204297f68a52fe04684
aazkgh <gg25hp17@naver.com>
2024-09-27 01:24:27 +0900

    fix: 서버 오류 수정

--- src/apis/supabase/postImgReview.ts
+++ src/apis/supabase/postImgReview.ts
@@ -1,22 +1,20 @@
 import { unitripSupabase } from '@/utils/supabaseClient';
 
-const postImgReview = async (place: number, imgs: File[]) => {
+const postImgReview = async (imgs: File[]) => {
   const imgUrls: string[] = [];
 
   // 각 파일을 Supabase Storage에 업로드
   for (const img of imgs) {
     const { error: uploadError } = await unitripSupabase.storage
       .from('REVIEW_IMAGES')
-      .upload(`${place}/${img.name}`, img, {
-        cacheControl: '3600',
-      });
+      .upload(`${img.name}`, img);
 
     if (uploadError) {
       throw new Error('이미지 업로드 과정에서 에러가 발생했습니다');
     }
 
     // 업로드한 파일의 URL 생성
-    const publicUrl = `${import.meta.env.VITE_STORAGE_URL}/${place}/${img.name}`;
+    const publicUrl = `${import.meta.env.VITE_STORAGE_URL}/${img.name}`;
     imgUrls.push(publicUrl);
   }
 
