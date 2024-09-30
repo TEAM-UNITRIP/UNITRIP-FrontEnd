4ce9fbfb5bf7e9776a9588fd935ae30dcc04e871
aazkgh <gg25hp17@naver.com>
2024-09-30 23:59:42 +0900

    fix: 에러 페이지로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { ErrorBoundary } from 'react-error-boundary';
-
-import Error from './components/Error';
 import Router from './Router';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 function App() {
   return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => {
-        return (
-          <Error
-            message={error.message}
-            resetErrorBoundary={resetErrorBoundary}
-          />
-        );
-      }}>
+    <>
       <GlobalStyles />
       <Router />
-    </ErrorBoundary>
+    </>
   );
 }
 
