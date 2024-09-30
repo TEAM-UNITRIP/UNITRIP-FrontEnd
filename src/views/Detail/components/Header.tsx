a5292b07cddda63e4041a93439c0517199c4b287
aazkgh <gg25hp17@naver.com>
2024-09-30 21:49:47 +0900

    feat: 즐찾 버튼 로그인 모달 추가

--- src/views/Detail/components/Header.tsx
+++ src/views/Detail/components/Header.tsx
@@ -2,23 +2,38 @@ import { css } from '@emotion/react';
 import { useState } from 'react';
 
 import { ArrowLeftIcon, HeartFilledIcon, HeartGrayIcon } from '@/assets/icon';
+import LoginModal from '@/components/LoginModal';
 
 function Header() {
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [activateModal, setActivateModal] = useState(false);
+
+  const isLoggedIn = sessionStorage.getItem('kakao_id');
 
   const favoriteOnClick = () => {
-    setIsFavorite(!isFavorite);
+    if (isLoggedIn) {
+      setIsFavorite(!isFavorite);
+    } else {
+      setActivateModal(true);
+    }
+  };
+
+  const closeModal = () => {
+    setActivateModal(false);
   };
 
   return (
-    <header css={headerContainer}>
-      <button type="button">
-        <ArrowLeftIcon />
-      </button>
-      <button type="button" onClick={favoriteOnClick}>
-        {isFavorite ? <HeartFilledIcon /> : <HeartGrayIcon />}
-      </button>
-    </header>
+    <>
+      <header css={headerContainer}>
+        <button type="button">
+          <ArrowLeftIcon />
+        </button>
+        <button type="button" onClick={favoriteOnClick}>
+          {isFavorite ? <HeartFilledIcon /> : <HeartGrayIcon />}
+        </button>
+      </header>
+      {activateModal && <LoginModal onClick={closeModal} />}
+    </>
   );
 }
 
